6c023fb6281a93a65907b082732f03e6e9e12f66
Pranay Jain <pranayjain@FVFGW2PBQ05N.local>
2022-02-14 17:38:49 +0530

    Updated multiple file feature

--- S_Loader/settings.py
+++ S_Loader/settings.py
@@ -79,12 +79,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  
         'NAME': 's_loader',  
         'USER': 'root',  
-        'PASSWORD': 'Searce123',  
+        'PASSWORD': 'Searce123', 
         'HOST': '127.0.0.1',  
         'PORT': '3306',  
-        'OPTIONS': {  
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-        }  
+        #'OPTIONS': {  
+        #    'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        #}  
     }  
 }  
 
--- uploadFile/urls.py
+++ uploadFile/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 
 urlpatterns = [
+    #path('',views.home, name='home'),
     path('',views.index, name='index'),
+    path('about',views.about, name='about'),
+    path('datamonitor',views.datamonitor, name='datamonitor'),
+#basic_form.html
  
 ]
