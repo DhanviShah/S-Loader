92d5a96b7f48fce00cd32e9f7fcb16a659290388
Pranay Jain <pranayjain@FVFGW2PBQ05N.bbrouter>
2022-02-18 23:24:19 +0530

    Updated option for dropping duplicates

--- uploadFile/views.py
+++ uploadFile/views.py
@@ -17,9 +17,10 @@ import os
 
 # Create your views here.
 
-def handle_uploaded_file(f):
+def handle_uploaded_file(f, flag):
         if f.name.endswith('.csv') or f.name.endswith('.xls') or f.name.endswith('.xlsx'):
-
+                
+                # flag=False
                 # perform the write operation into the folder (upload at server)
                 with open('uploadFile/upload/' + f.name, 'wb+') as destination: 
                         for chunk in f.chunks():
@@ -30,7 +31,8 @@ def handle_uploaded_file(f):
                         # update mysql database with csv
                         df = pd.read_csv('uploadFile/upload/' + f.name, header=[0])
                         # remove duplicates if two rows match
-                        df = df.drop_duplicates()
+                        if flag:
+                                df = df.drop_duplicates()
 
                         engine = create_engine('mysql+mysqldb://root:Searce123@localhost:3306/s_loader')
                         table_name = os.path.splitext(f.name)[0]
@@ -38,11 +40,15 @@ def handle_uploaded_file(f):
 
                 elif f.name.endswith('.xls') or f.name.endswith('.xlsx'):
                         df = pd.read_excel('uploadFile/upload/' + f.name, engine = 'openpyxl')
-                        df = df.drop_duplicates()
+                        if flag:
+                                df = df.drop_duplicates()
                         engine = create_engine('mysql+mysqldb://root:Searce123@localhost:3306/s_loader')
                         table_name = os.path.splitext(f.name)[0]
                         df.to_sql(name = table_name, con = engine, index_label= 'id')
 
+                # duplicate = df[df.duplicated(keep = 'False')]
+                # if duplicate:
+                #         flag = True
                 """"
                 # add id attribute as primary key
                 #m=sql.connect(host="localhost",user="root",password="Searce123",database="s_loader")
@@ -50,7 +56,8 @@ def handle_uploaded_file(f):
                 query = "ALTER TABLE {} ADD COLUMN id INT PRIMARY KEY NOT NULL AUTO_INCREMENT FIRST".format(table_name)
                 cursor.execute(query)
                 m.commit()"""
-
+                
+                # return flag
 
         else:
                 print("not an excel file.")
@@ -61,18 +68,22 @@ def index(request):
         if request.POST:
                 form = FileForm(request.POST, request.FILES)
                 files = request.FILES.getlist('files')
+                temp_dup = request.POST.get('drop_dup', False)
+                # temp_display = request.POST.get('drop_dup', False)
                 if form.is_valid():
-                        if len(files)>5:
-                                context = {'msg' : 'You can only upload 5 files at max.'}
+                        if len(files)>4:
+                                context = {'msg' : 'You can only upload 4 files at max.'}
                                 
                         else:
                                 a = []
                                 for i,f in enumerate(files):
                                         #print(f.name)
-                                        handle_uploaded_file(f)
+                                        handle_uploaded_file(f, temp_dup)
                                         a.append(f.name)
-                                
-                                context = {'msg' : 'Following files successfully uploaded.'}
+                                        
+                                        
+
+                                context={'msg':'Following files successfully uploaded.'}
                                 context['names'] = a
                                         
         else :
